7336dfafbbea387a12ddc42975d3028fbc8c52b0
Aled Vaghela <aledvaghela@gmail.com>
2025-03-04 15:19:55 +0000

    added more complete testing for snatchable word generator

--- tests/test_snatchable_word_generator.cpp
+++ tests/test_snatchable_word_generator.cpp
@@ -18,7 +18,7 @@ TEST_F(TestSnatchableWordGenerator, SingleLettersNoSnatchable) {
 	EXPECT_EQ(std::size(snatchable), 0);
 }
 
-TEST_F(TestSnatchableWordGenerator, SingleLettersSnatchable1) {
+TEST_F(TestSnatchableWordGenerator, SingleLettersSnatchable) {
 	std::vector<std::string> words{"P" , "I", "T", "R"};
 	std::vector<std::string> snatchable = swg.generateSnatchableWords(words);
 	EXPECT_EQ(std::size(snatchable), 4);
@@ -26,4 +26,20 @@ TEST_F(TestSnatchableWordGenerator, SingleLettersSnatchable1) {
 	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "TIP"), snatchable.end()) << "TIP expected to be snatchable";
 	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "RIP"), snatchable.end()) << "RIP expected to be snatchable";
 	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "TRIP"), snatchable.end()) << "TRIP expected to be snatchable";
+}
+
+TEST_F(TestSnatchableWordGenerator, MultipleLettersSnatchable) {
+	std::vector<std::string> words{ "PET", "RAM" };
+	std::vector<std::string> snatchable = swg.generateSnatchableWords(words);
+	EXPECT_EQ(std::size(snatchable), 1);
+	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "TAMPER"), snatchable.end()) << "TAMPER expected to be snatchable";
+}
+
+TEST_F(TestSnatchableWordGenerator, ComplexExample) {
+	std::vector<std::string> words{ "PET", "RAM", "E"};
+	std::vector<std::string> snatchable = swg.generateSnatchableWords(words);
+	EXPECT_EQ(std::size(snatchable), 3);
+	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "TAMPER"), snatchable.end()) << "TAMPER expected to be snatchable";
+	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "MARE"), snatchable.end()) << "MARE expected to be snatchable";
+	EXPECT_NE(std::find(snatchable.begin(), snatchable.end(), "REAM"), snatchable.end()) << "REAM expected to be snatchable";
 }
